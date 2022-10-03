8c5b26d37cd849ea8fe1d4e1ed3dfbee2ae64637
k-miyazoe <nk.miyazoe@gmail.com>
2022-10-03 10:53:58 +0900

    vue.config.jsを修正

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "./question-board-saga",
+  // publicPath: "./question-board-saga"に変更したらlocalでも表示されなくなった
+  publicPath: "./",
   transpileDependencies: ["vuetify"],
 });
